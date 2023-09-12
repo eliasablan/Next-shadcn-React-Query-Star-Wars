d9db403078e1b8f48cec8d949d22839d7a92b9e7
Elias Guillermo <eliasablan93@gmail.com>
2023-09-12 15:12:19 -0400

    fix toggle theme button right margin to header

--- src/components/ButtonThemeToggle.js
+++ src/components/ButtonThemeToggle.js
@@ -49,7 +49,6 @@ export function ModeToggle() {
       onClick={() => setTheme(theme == "dark" ? "light" : "dark")}
       variant="secondary"
       size="icon"
-      className="mr-4"
     >
       <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
