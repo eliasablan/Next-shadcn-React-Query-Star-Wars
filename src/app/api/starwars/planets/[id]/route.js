000d5e55a5917caaf36b7aa4c1091e5f3f194bf2
Elias Ablan <eliasablan93@gmail.com>
2023-09-22 00:16:29 -0400

    api routes updated

--- src/app/api/starwars/planets/[id]/route.js
+++ src/app/api/starwars/planets/[id]/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const url = new URL("https://swapi.dev/api/people/");
+export async function GET(request, route) {
+  const homeworldId = route.params.id;
+  const url = new URL(`https://swapi.dev/api/planets/${homeworldId}`);
   const response = await fetch(url);
   const data = await response.json();
 
