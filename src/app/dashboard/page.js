761558f5f1f51b6fad8553eb498d8b364f95b16d
Elias Ablan <eliasablan93@gmail.com>
2023-09-24 19:45:53 -0400

    redux toolkit removed from project

--- src/app/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-import { CharactersTable } from "@/components/CharactersTable";
-
-export default function Page() {
-  return (
-    <>
-      <CharactersTable />
-    </>
-  );
-}
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,54 +1,8 @@
-import axios from "axios";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import useSWR from "swr";
-
-import { updateCharacters } from "@/reducers/starWarsSlice";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
-}
-
-export async function fetchCharacters(dispatch) {
-  try {
-    console.log("🟡 Started fetching characters");
-    const response = await axios.get("https://swapi.dev/api/people");
-    const characters = response.data;
-    const tableData = characters.results.map((character) => {
-      const homeworld = fetchPlanet(character.homeworld);
-      return {
-        name: character.name,
-        birth_year: character.birth_year,
-        homeworld: homeworld,
-      };
-    });
-    console.log("🟡 characters fetched");
-    if (tableData) {
-      dispatch(updateCharacters(tableData));
-      console.log("🟢 store SWcharacters updated", tableData);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export async function fetchPlanet(url) {
-  const response = await axios.get(url);
-  return response.data.name;
-}
-
-export function useSWCharacters() {
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(
-    "https://swapi.dev/api/people/",
-    fetcher
-  );
-
-  return {
-    data,
-    isLoading,
-    isError: error,
-  };
 }
 
 const getCharacters = async () => {
